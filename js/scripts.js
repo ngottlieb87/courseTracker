4e79acb4a3c7dfc5c2e115c49c43c1702f3f2b54
ngottlieb87 <nogpdx@gmail.com>
2017-08-11 10:13:46 -0700

    add button for submission and initial variables for js

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
-$(document).ready(function{
-  
+$(document).ready(function(){
+  $("form#userInputs").submit(function(event){
+    event.Default();
+    
+  var choice1 = $("#expirience").val();
+  var choice2 = $("#why").val();
+  var choice3 = $("#environment").val();
+  var choice4 = $("#design").val();
+  var choice5 = $("#challenges").val();
+
+    console.log(choice1,choice2,choice3,choice4,choice5);
+
+  });
 });
