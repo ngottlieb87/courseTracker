a2bfc7ccbfa28d95aed0a651c52a2db603351d7d
ngottlieb87 <nogpdx@gmail.com>
2017-08-11 10:46:17 -0700

    create first if statement and parseInt selections

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,16 @@ $(document).ready(function(){
   $("form#userInputs").submit(function(event){
     event.preventDefault();
 
-  var choice1 = $("#expirience").val();
-  var choice2 = $("#why").val();
-  var choice3 = $("#environment").val();
-  var choice4 = $("#design").val();
-  var choice5 = $("#challenges").val();
+  var choice1 = parseInt($("#expirience").val());
+  var choice2 = parseInt($("#why").val());
+  var choice3 = parseInt($("#environment").val());
+  var choice4 = parseInt($("#design").val());
+  var choice5 = parseInt($("#challenges").val());
   var course;
+  if(choice1 === 1 && choice2 === 1 && choice3 === 1 && choice4 === 1 && choice5 ===1){
+    course = "Your best fit is ruby";
+
+  };
 
     $("#output").text(course).show();
   });
