8a9efaecd420d28538c99e05395b94b2aaa7d482
ngottlieb87 <nogpdx@gmail.com>
2017-08-11 11:13:13 -0700

    create second outcome for design base on choices

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,13 @@ $(document).ready(function(){
   var choice4 = parseInt($("#design").val());
   var choice5 = parseInt($("#challenges").val());
   var course;
-  if(choice1 === 1 && choice2 === 1 && choice3 === 1 && choice4 === 1 && choice5 ===1){
-    course = "Your best fit is ruby";
-
-  };
+  if(choice1 === 1 && choice2 === 1 && choice3 === 1 && choice4 === 2 && choice5 === 3) {
+    course = "Your best fit is Ruby and Rails! You are interested in building web applications and using a mac. Macs has ruby built in and ready to go! It also looks like you enjoy working with others which is a big plus when trying to learn a new language. Go for it!";
+  } else if(choice1 === 2 && choice2 === 5 && choice3 === 2 && choice4 === 1 && choice5 === 1){
+      course = "Your best fit looks like it would be CSS/Design! You have a little expirience which is great when going into a new learning expirience. From your choices it seems like you enjoy aesthetics over the fucntionality of a website, however they often overlap! It also seems like you enjoy to take on challenges by yourself which is a great talent to have, but keep in mind that working with others opens you up to new learning oppurtunities that you might not have achieved by yourself.";
+    }else if(choice1 === 2 && choice2 === 5 && choice3 === 2 && choice4 === 1 && choice5 === 1){
+      
+    };
 
     $("#output").text(course).show();
   });
